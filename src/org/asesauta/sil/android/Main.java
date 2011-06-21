220d4a4738c6a7e28af0214e23527db86d176b7b
jorge_svn <jorge_svn@f5313253-aa36-0410-a683-1d7e1fc83e77>
2011-06-21 21:28:57 +0000

    guardo estado
    git-svn-id: svn://209.20.83.121/var/svn/repo/Silabeador-android@239 f5313253-aa36-0410-a683-1d7e1fc83e77

--- src/org/asesauta/sil/android/Main.java
+++ src/org/asesauta/sil/android/Main.java
@@ -1,5 +1,6 @@
 package org.asesauta.sil.android;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.asesauta.anapalabra.Silabeador;
@@ -18,6 +19,9 @@ public class Main extends Activity implements OnClickListener {
     private final String TAG = "silabas";
     private final String SILABAS = "sílabas: ";
     private final String TONICA = "sílaba tónica: ";
+    private String palabra;
+    private ArrayList<String> silabas;
+    private String tonica;
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,24 +35,57 @@ public class Main extends Activity implements OnClickListener {
         
         Button button = (Button) findViewById(R.id.Button01);
         button.setOnClickListener(this);
+        
+        if (savedInstanceState!=null) {
+    		Log.i(TAG, "recuperando la instancia");
+    		EditText input = (EditText) findViewById(R.id.EditText01);
+    		palabra = savedInstanceState.getString("palabra");
+    		tonica = savedInstanceState.getString("tonica");
+    		silabas = savedInstanceState.getStringArrayList("silabas");
+    		input.setText(palabra);
+    		updateScreen();
+        }
     }
+    
+	@Override
+	protected void onSaveInstanceState(Bundle outState)
+	{
+		super.onSaveInstanceState(outState);
+		Log.i(TAG, "guardando el estado");
+		outState.putString("palabra", palabra);
+		outState.putStringArrayList("silabas", silabas);
+		outState.putString("tonica", tonica);
+	}
 
 	@Override
 	public void onClick(View v)
 	{
 		Log.i(TAG, "click");
-		LinearLayout silabasResult = (LinearLayout) findViewById(R.id.LinearLayoutSilabas);
-		silabasResult.setVisibility(LinearLayout.VISIBLE);
-		LinearLayout tonicaResult = (LinearLayout) findViewById(R.id.LinearLayoutTonica);
-		tonicaResult.setVisibility(LinearLayout.VISIBLE);
 		EditText input = (EditText) findViewById(R.id.EditText01);
 		Silabeador silabeador = new Silabeador();
-		List<String> silabas = silabeador.silabear(input.getText().toString().trim());
-		String tonica = silabas.get(silabeador.tonica(silabas));
-		TextView outputSilabas = (TextView) findViewById(R.id.TextView01);
-		outputSilabas.setText(SILABAS+formatSilabas(silabas));
-		TextView outputTonica = (TextView) findViewById(R.id.TextView02);
-		outputTonica.setText(TONICA+tonica);
+		palabra = input.getText().toString();
+		silabas = silabeador.silabear(palabra.trim());
+		tonica = silabas.get(silabeador.tonica(silabas));
+		updateScreen();
+	}
+	
+	/**
+	 * Se llama al pulsar el botón 'silabear' o al restaurar el estado.
+	 */
+	public void updateScreen()
+	{
+		if (silabas!=null && silabas.size()>0) {
+			LinearLayout silabasResult = (LinearLayout) findViewById(R.id.LinearLayoutSilabas);
+			silabasResult.setVisibility(LinearLayout.VISIBLE);
+			TextView outputSilabas = (TextView) findViewById(R.id.TextView01);
+			outputSilabas.setText(SILABAS+formatSilabas(silabas));
+		}
+		if (tonica!=null) {
+			TextView outputTonica = (TextView) findViewById(R.id.TextView02);
+			outputTonica.setText(TONICA+tonica);
+			LinearLayout tonicaResult = (LinearLayout) findViewById(R.id.LinearLayoutTonica);
+			tonicaResult.setVisibility(LinearLayout.VISIBLE);
+		}
 	}
 	
 	public String formatSilabas(List<String> silabas) {
